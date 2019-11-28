da9c9201fa15f86f921e91c64c1aeaa378b0f6f6
Diego Rodrigues <Diegophotography@gmail.com>
2019-11-28 12:04:38 -0500

    alpha

---
+++ FollowTheWhiteRabbit/Hub/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace FollowTheWhiteRabbit
+{
+    public class ChatHub : Hub
+    {
+        public async Task SubmitChat(string message)
+        {
+            await Clients.All.SendAsync("UpdateChat", message);
+        }
+
+        public async override Task OnConnectedAsync()
+        {
+            await Clients.Client(Context.ConnectionId).SendAsync("UpdateChat", $"Operator: You are plugged in.");
+            await Clients.AllExcept(Context.ConnectionId).SendAsync("UpdateChat", $"Operator: A new user plugged in.");
+            await base.OnConnectedAsync();
+        }
+    }
+}
---
+++ FollowTheWhiteRabbit/wwwroot/js/site.js
@@ -0,0 +1,7 @@
+﻿window.addNewChat = (textBoxElem, chatBoxElem, chatMessage) => {
+    var para = document.createElement("p");
+    var node = document.createTextNode(chatMessage);
+    para.appendChild(node);
+    chatBoxElem.appendChild(para);
+    textBoxElem.focus();
+};
