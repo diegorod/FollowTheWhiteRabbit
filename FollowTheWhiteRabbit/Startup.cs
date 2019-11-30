7cbdc961182fb3453b0b310e6ae8ead5b140ba36
Diego Rodrigues <Diegophotography@gmail.com>
2019-11-30 09:44:22 -0500

    redundant package removed

--- FollowTheWhiteRabbit/Startup.cs
+++ FollowTheWhiteRabbit/Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Blazor.Extensions;
 
 namespace FollowTheWhiteRabbit
 {
@@ -30,7 +29,6 @@ namespace FollowTheWhiteRabbit
             services.AddRazorPages();
 
             services.AddServerSideBlazor();
-            services.AddTransient<HubConnectionBuilder>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
